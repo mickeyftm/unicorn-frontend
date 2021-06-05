a6eec9f0e6fb264233dd3f8f1760ab20dc8933fd
Li Chuan <udefi58@gmail.com>
2021-06-06 03:31:40 +0800

    init

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -61,7 +61,7 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'Salmon Swap')}
+          {TranslateString(576, 'Unicorn DeFi')}
         </Heading>
         <Text>{TranslateString(578, 'Yield farm on BSC.')}</Text>
       </Hero>
